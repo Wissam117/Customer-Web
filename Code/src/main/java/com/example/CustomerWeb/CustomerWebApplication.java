792ec88e2364fe1697545e28713f0987671d5a8f
Wissam <mwissam11@gmail.com>
2025-07-10 17:13:07 +0500

    Appropriate Packages and Initial code

--- Code/src/main/java/com/example/CustomerWeb/CustomerWebApplication.java
+++ Code/src/main/java/com/example/CustomerWeb/CustomerWebApplication.java
@@ -1,13 +1,13 @@
-package com.example.First_Project;
+package com.example.CustomerWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstProjectApplication {
+public class CustomerWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstProjectApplication.class, args);
+		SpringApplication.run(CustomerWebApplication.class, args);
 	}
 
 }
---
+++ Code/src/test/java/com/example/CustomerWeb/CustomerWebApplication.java
@@ -0,0 +1,11 @@
+package com.customerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebApplication.class, args);
+	}
+}
